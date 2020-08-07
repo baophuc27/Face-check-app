38d140d53e5769e06d9c58de790ede603b416a27
Bao Phuc <phucphucbao2710@gmail.com>
2020-08-07 10:30:15 +0700

    add image handler

---
+++ image_handler/handler.py
@@ -0,0 +1,24 @@
+from cryptography.fernet import Fernet
+
+
+
+class Handler(object):
+
+    def __init__(self,url):
+        self.key = self.load_key()
+        self.url = url
+        self.encrypted_url = self.encrypt_url()
+
+    def load_key():
+        return open("secret.key","rb").read()
+
+    def encrypt_url(self):
+
+        encoded_message = self.url.encode()
+        f = Fernet(self.key)
+        encrypted_message = f.encrypt(encoded_message)
+
+        return encrypted_message.decode("utf-8")
+
+    def get_url(self):
+        return self.encrypted_url
