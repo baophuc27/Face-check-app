227e48e1157607582e1b3067c2cf4ded0dc17f2a
Bao Phuc <phucphucbao2710@gmail.com>
2020-08-10 15:33:36 +0700

    temp commit

--- web.py
+++ web.py
@@ -18,7 +18,7 @@ vs = VideoStream(src=0).start()
 time.sleep(2.0)
 # detector = Detector()
 hostname = "http://0.0.0.0:5000/"
-FACE_DETECTOR_SERVER = "http://face-detector.azurewebsites.net/detect?img_url="
+FACE_DETECTOR_SERVER = "http://0.0.0.0:8002/detect?img_url="
 
 # Google cloud storage to store image
 storage = Storage().storage
@@ -33,17 +33,11 @@ def index():
 
 
 
-def detect_face(frame):
-    global vs, outputFrame
-    outputFrame = detector.processs(frame)
-    return outputFrame
-
-
 def gen_image():
     global vs, frame
     while True:
         frame = vs.read()
-        frame = cv2.flip(frame, 1)
+        # frame = cv2.flip(frame, 1)
         if frame is not None:
             (flag, encodedImage) = cv2.imencode(".jpg", frame)
 
@@ -76,26 +70,13 @@ def download():
     storage.child("user1/image").download("image.jpg")
     return render_template("index.html")
 
-@app.route("/getfb")
-def getfb():
-    res= database.child('user').get()
-    users=[]
-    embeddings=[]
-    for user in res.each():
-        val=user.val()
-        embedding = val['embedding']
-        embedding=np.fromstring(embedding[1:-1],dtype='float',sep=' ').reshape(512,1)
-        users.append({"link":val["link"],"time":val["time"]})
-        embeddings.append(embedding)
 
-    indexes=get_similar_index(embeddings,embedding)
-    data = [users[index[0]] for index in indexes]
-    json_data = json.dumps(data)
-    json_data = json.loads(json_data)
-    return render_template("index.html",data = data)
 
 def face_distance(face_encodings, face_to_compare):
+    print(face_encodings.shape)
+    print(face_to_compare.shape)
     face_dist_value = np.linalg.norm(face_encodings - face_to_compare, axis=1)
+    print(face_dist_value)
     return face_dist_value
 
 def get_similar_index(face_encodings,face_to_compare):
@@ -103,6 +84,17 @@ def get_similar_index(face_encodings,face_to_compare):
         return np.empty((0))
     indexes = np.argwhere(face_distance(face_encodings,face_to_compare) < configs.THRESHOLD )
     return indexes
+
+
+@app.route("/detect", methods=["POST"])
+def detect():
+    r = requests.get(hostname + "getdata")
+    data = r.json()
+    database.child("user").push(data)
+
+    similar_data = get_similar_data()
+
+    return render_template("index.html",data = similar_data)
 
 @app.route("/getdata")
 def get_data():
@@ -123,16 +115,26 @@ def get_embedding(url):
     data=res.json()
     emb=data["embedding"]
     embedding=np.fromstring(emb[1:-1],dtype=float,sep=' ')
-    return embedding.reshape((1,512))
+    np.save("emb",embedding)
+    return embedding.reshape(512)
 
-
-@app.route("/detect", methods=["POST"])
-def detect():
-    r = requests.get(hostname + "getdata")
-    data = r.json()
-    database.child("user").push(data)
-    return render_template("index.html")
-
+def get_similar_data():
+    res= database.child('user').get()
+    users=[]
+    embeddings=[]
+    for user in res.each():
+        val=user.val()
+        embedding = val['embedding']
+        print(embedding[1:-1])
+        embedding=np.fromstring(embedding[1:-1],dtype='float',sep=' ').reshape(512)
+        users.append({"link":val["link"],"time":val["time"]})
+        embeddings.append(embedding)
+    embeddings=np.asarray(embeddings)
+    indexes=get_similar_index(embeddings[:-1],embedding)
+    # print("distance")
+    # print(face_distance(np.load("emb.npy"),embedding))
+    data = [users[index[0]] for index in indexes]
+    return data
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
